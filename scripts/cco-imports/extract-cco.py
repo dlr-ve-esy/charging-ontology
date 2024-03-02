f0239d836a58cb16d407de6d9775694740e34f9d
Arellano Ruiz, Eugenio Salvador <eugenio.arellanoruiz@dlr.de>
2024-03-02 10:10:54 +0100

    Reworking CCO imports, currently broken

--- scripts/cco-imports/extract-cco.py
+++ scripts/cco-imports/extract-cco.py
@@ -1,36 +1,142 @@
+# %% [markdown]
+# # Extraction of CCO modules
 # %%
 import requests
 import subprocess as sp
 from pathlib import Path
-
-# NOTE: Versioning in CCO is confusing, is very tricky to import the latest
-# released version, that is why we have to bind it to a commit
+import tempfile
 
 BASE_IRI = "http://openenergy-platform.org/ontology"
-COMMIT = "a99731c59859047e84253d15f8eba438b0aa71da"
-NAME = "MergedAllCoreOntology-v1.5-2024-02-14.ttl"
+VERSION = "master"
+
 ONTOLOGY_BASE = (
     "https://raw.githubusercontent.com/CommonCoreOntology/"
-    f"CommonCoreOntologies/{COMMIT}/cco-merged/{NAME}"
+    "CommonCoreOntologies/{}/{}.ttl"
 )
 UPPER_TERM = "http://purl.obolibrary.org/obo/BFO_0000003"
 CLASS_IRI = "http://purl.obolibrary.org/obo/BFO_0000144"
-NEW_IRI = f"{BASE_IRI}/chio/imports/cco-extracted.ttl"
-VERSION_IRI = f"{BASE_IRI}/chio/dev/imports/cco-extracted.ttl"
+NEW_IRI = "{}/chio/imports/{}.ttl"
+VERSION_IRI = "{}/chio/dev/imports/{}.ttl"
 TERM_FILE = "cco-w-hierarchy.txt"
+
 # Change these paths according to your setup.
 ROBOT_PATH = "../../robot.jar"
 CCO = "tmp/MergedAllCoreOntology.ttl"
 TARGET = "../../src/imports/cco-extracted.ttl"
 Path("tmp").mkdir(exist_ok=True)
 # %%
-if not Path(CCO).exists():
-    with open(CCO, "wb") as local:
-        response = requests.get(ONTOLOGY_BASE)
-        if response.status_code == 200:
-            local.write(response.content)
-else:
-    print(f"The file {CCO} already exists, you are good to go.")
+
+
+def download_ontology_if_missing(ONTOLOGY):
+    """Helper function to download raw ontologies in a temporary folder.
+
+    Args:
+        ONTOLOGY (_type_): _description_
+    """
+    temporary_path = Path("tmp").joinpath(f"{ONTOLOGY}.ttl")
+    if not Path(temporary_path).exists():
+        with open(temporary_path, "wb") as local:
+            response = requests.get(ONTOLOGY_BASE.format(VERSION, ONTOLOGY))
+            if response.status_code == 200:
+                local.write(response.content)
+    else:
+        print(f"The file {temporary_path} already exists, you are good to go.")
+    return temporary_path
+
+
+# %%
+def extract_mierot(
+    input: str,
+    output: str,
+    lower_terms: str,
+    intermediates="all",
+    upper_term: str = "owl:Thing",
+):
+    extract_call = (
+        "java -jar {jar} extract "
+        "--input {input} "
+        "--method MIREOT "
+        "--upper-term {upper_term} "
+        "--lower-terms {lower_terms} "
+        "--intermediates {intermediates} "
+        "--output {output}"
+    )
+    with open(Path("tmp").joinpath("temp.txt"), "w") as fp:
+        for term in lower_terms:
+            fp.write(term + "\n")
+    debug_string = extract_call.format(
+        jar=Path(ROBOT_PATH).resolve().as_posix(),
+        input=Path(input).resolve().as_posix(),
+        lower_terms=Path("tmp").joinpath("temp.txt").resolve().as_posix(),
+        upper_term=upper_term,
+        intermediates=intermediates,
+        output=Path(output).resolve().as_posix(),
+    )
+    code = sp.call(
+        debug_string,
+        shell=True,
+    )
+    if code != 0:
+        print(debug_string)
+        raise IOError(f"Something went wrong with call: {debug_string}")
+    return code
+
+
+# %% [markdown]
+# ## Extract classes from the event ontology
+# %%
+event_ontology = "EventOntology"
+event_ontology = download_ontology_if_missing("EventOntology")
+# %%
+# Stasis
+if not Path("tmp").joinpath("eo_stasis.ttl").exists():
+    upper_term = "http://purl.obolibrary.org/obo/BFO_0000015"
+    lower_terms = [
+        "http://www.ontologyrepository.com/CommonCoreOntologies/StableOrientation",
+        "http://www.ontologyrepository.com/CommonCoreOntologies/StasisOfDisposition",
+        "http://www.ontologyrepository.com/CommonCoreOntologies/StasisOfFunction",
+        "http://www.ontologyrepository.com/CommonCoreOntologies/StasisOfRole",
+    ]
+    extract_mierot(
+        input=event_ontology,
+        output=Path("tmp").joinpath("eo_stasis.ttl"),
+        lower_terms=lower_terms,
+        upper_term=upper_term,
+    )
+# %%
+# Process profiles
+if not Path("tmp").joinpath("eo_process_profiles.ttl").exists():
+    upper_term = "http://purl.obolibrary.org/obo/BFO_0000144"
+    lower_terms = [
+        "http://www.ontologyrepository.com/CommonCoreOntologies/MaximumPower"
+    ]
+    extract_mierot(
+        input=event_ontology,
+        output=Path("tmp").joinpath("eo_process_profiles.ttl"),
+        lower_terms=lower_terms,
+        upper_term=upper_term,
+    )
+# %% [markdown]
+# ## Exrract vehicle classes
+# These axioms are very close to the ones from the OEO. I have to do some
+# adjustments to ensure compatibility.
+# Vehicle taxonomy from CCO
+# Vehicle IRI from OEO and electric/ICE definitions from the OEO
+# %%
+artifact_ontology = download_ontology_if_missing("ArtifactOntology")
+if not Path("tmp").joinpath("eo_artifacts.ttl").exists():
+    upper_term = "http://purl.obolibrary.org/obo/BFO_0000040"
+    lower_terms = [
+        "http://www.ontologyrepository.com/CommonCoreOntologies/ElectricMotor",
+        "http://www.ontologyrepository.com/CommonCoreOntologies/CompressionIgnitionEngine",
+        "http://www.ontologyrepository.com/CommonCoreOntologies/SparkIgnitionEngine",
+    ]
+    extract_mierot(
+        input=artifact_ontology,
+        output=Path("tmp").joinpath("eo_artifacts.ttl"),
+        lower_terms=lower_terms,
+        upper_term=upper_term,
+    )
 # %%
 if not Path(TARGET).exists():
     extract_call = 'java -jar {jar} \
@@ -93,7 +199,6 @@ sp.call(
     ),
     shell=True,
 )
-
 # %%
 annotate_call = "java -jar {jar} \
 annotate --input {input} \
