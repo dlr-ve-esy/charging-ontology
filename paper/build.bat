2cf7d94d49b5df235c94ba45b193589a941bf7ad
Arellano Ruiz, Eugenio Salvador <eugenio.arellanoruiz@dlr.de>
2024-03-06 08:12:48 +0100

    Added paper draft

---
+++ paper/build.bat
@@ -0,0 +1,7 @@
+﻿pdflatex -interaction nonstopmode -shell-escape paper.tex
+bibtex paper
+pdflatex -interaction nonstopmode -shell-escape paper.tex
+pdflatex -interaction nonstopmode -shell-escape paper.tex
+pdflatex -interaction nonstopmode -shell-escape paper.tex
+qpdf --linearize --newline-before-endstream paper.pdf tmp/paper.pdf
+mv tmp/paper.pdf paper.pdf
