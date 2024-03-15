4690a792035846ea8b65539d0885e515356e5eca
Arellano Ruiz, Eugenio Salvador <eugenio.arellanoruiz@dlr.de>
2024-03-15 17:04:23 +0100

    Added CQS with instantiation

--- tests/test_competency_questions.py
+++ tests/test_competency_questions.py
@@ -11,6 +11,7 @@ ROBOT_JAR = Path(CWD).joinpath("robot.jar").as_posix()
 CATALOG_PATH = Path(CWD).joinpath("src/catalog-v001.xml")
 ONTOLOGY_PATH = Path(CWD).joinpath("src").joinpath("chio.ttl").as_posix()
 COMPETENCY_QUESTION_DIRECTORY = Path(CWD).joinpath("tests/cqs")
+INSTANCE_PATH = Path(CWD).joinpath("tests/cq_instances")
 
 
 def pytest_generate_tests(metafunc):
@@ -37,9 +38,24 @@ def check_competency_question(query, ontology, tmp):
     Returns:
         (boolean): Returns True if the competency question was sucessfully checked against the ontology.
     """
+    # instance_list = [
+    #         p
+    #         for p in glob(
+    #             INSTANCE_PATH.as_posix() + "/**/*.ttl", recursive=True
+    #         )
+    #     ]
+    query = Path(query).as_posix()
+    instance_check = query.replace(
+        COMPETENCY_QUESTION_DIRECTORY.as_posix(), INSTANCE_PATH.as_posix()
+    ).replace(".rq", ".ttl")
+    if Path(instance_check).exists():
+        instance_input = f" --input {instance_check} "
+    else:
+        instance_input = ""
     query_call = (
-        f"java -jar {ROBOT_JAR} merge --input {ontology} --catalog {CATALOG_PATH.as_posix()} "
-        + f"query --format ttl --query {query} {tmp}"
+        f"java -jar {ROBOT_JAR} merge --input {ontology} {instance_input}"
+        + f"--catalog {CATALOG_PATH.as_posix()} "
+        + f"query --format ttl --query {query} {tmp.as_posix()}"
     )
     response = sp.call(query_call)
     if response != 0:
