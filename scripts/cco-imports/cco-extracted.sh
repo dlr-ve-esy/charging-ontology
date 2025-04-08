c2e0c79370f28b34307a020d3b280cd429acd65e
Eugenio Salvador Arellano Ruiz <eugenio.arellanoruiz@dlr.de>
2025-04-08 23:02:31 +0200

    Added more information content entities

--- scripts/cco-imports/cco-extracted.sh
+++ scripts/cco-imports/cco-extracted.sh
@@ -25,13 +25,6 @@ cco_new_version_iri="${ontology_name}/dev/imports"
 echo "${cco_base}"
 test -f ${tmpdir}/EventOntology.ttl && echo "${tmpdir}/EventOntology.ttl already exists." || curl -L -o ${tmpdir}/EventOntology.ttl ${cco_base}/EventOntology.ttl
 
-# Extractions from Information EntityOntology
-test -f ${tmpdir}/InformationEntityOntology.ttl && echo "${tmpdir}/InformationEntityOntology.ttl already exists." || curl -L -o ${tmpdir}/InformationEntityOntology.ttl ${cco_base}/InformationEntityOntology.ttl
-
-
-# Extractions from Information EntityOntology
-test -f ${tmpdir}/InformationEntityOntology.ttl && echo "${tmpdir}/InformationEntityOntology.ttl already exists." || curl -L -o ${tmpdir}/InformationEntityOntology.ttl ${cco_base}/InformationEntityOntology.ttl
-
 # Extractions from Artifact Ontology
 echo "downloading ${cco_base}/ArtifactOntology.ttl"
 test -f ${tmpdir}/ArtifactOntology.ttl && echo "${tmpdir}/ArtifactOntology.ttl already exists." || curl -L -o ${tmpdir}/ArtifactOntology.ttl ${cco_base}/ArtifactOntology.ttl
@@ -83,12 +76,16 @@ java -jar robot.jar remove --catalog ${tmpdir}/catalog.xml --input ${tmpdir}/Geo
 
 java -jar robot.jar remove --catalog ${tmpdir}/catalog.xml --input ${tmpdir}/AgentOntology.ttl --select imports extract --method MIREOT  --imports exclude --upper-term https://www.commoncoreontologies.org/ont00000472 --lower-terms ${this_wd}/geo_tree.txt --intermediates all --output ${tmpdir}/geo_tree.ttl
 
+java -jar robot.jar remove --catalog ${tmpdir}/catalog.xml --input ${tmpdir}/InformationEntityOntology.ttl --select imports extract --method MIREOT  --imports exclude --upper-term https://www.commoncoreontologies.org/ont00000958 --lower-terms ${this_wd}/ieo_mireot.txt --intermediates all --output ${tmpdir}/ieo_mireot_base.ttl
+
 java -jar robot.jar remove --catalog ${tmpdir}/catalog.xml --input ${tmpdir}/InformationEntityOntology.ttl --select imports extract --method subset --term-file ${this_wd}/ieo_ops.txt --imports exclude --output ${tmpdir}/ieo_ops_base.ttl
 
 java -jar robot.jar rename --input ${tmpdir}/ieo_ops_base.ttl --mappings ${this_wd}/ieo_ops_mappings.csv --allow-missing-entities true --output ${tmpdir}/ieo_ops.ttl
 
+java -jar robot.jar rename --input ${tmpdir}/ieo_mireot_base.ttl --mappings ${this_wd}/ieo_ops_mappings.csv --allow-missing-entities true --output ${tmpdir}/ieo_entities.ttl
+
 # Merging together
 
-java -jar robot.jar merge --input ${tmpdir}/geo_tree.ttl --input ${tmpdir}/geo_base.ttl --input ${tmpdir}/ao_artifacts.ttl --input ${tmpdir}/ao_vehicles.ttl --input ${tmpdir}/ao_facility.ttl --input ${tmpdir}/ao_facility_classes.ttl --input ${tmpdir}/eo_stasis.ttl --input ${tmpdir}/eo_process_profiles.ttl --input ${tmpdir}/eo_change.ttl --input ${tmpdir}/ao_infrastructure.ttl --input ${tmpdir}/ao_electrical.ttl --input ${tmpdir}/ieo_ops.ttl annotate --annotation rdfs:comment "This is an extract of the Common Core Ontologies: https://github.com/CommonCoreOntology/CommonCoreOntologies " --output src/imports/cco-extracted.ttl
+java -jar robot.jar merge --input ${tmpdir}/geo_tree.ttl --input ${tmpdir}/geo_base.ttl --input ${tmpdir}/ao_artifacts.ttl --input ${tmpdir}/ao_vehicles.ttl --input ${tmpdir}/ao_facility.ttl --input ${tmpdir}/ao_facility_classes.ttl --input ${tmpdir}/eo_stasis.ttl --input ${tmpdir}/eo_process_profiles.ttl --input ${tmpdir}/eo_change.ttl --input ${tmpdir}/ao_infrastructure.ttl --input ${tmpdir}/ao_electrical.ttl --input ${tmpdir}/ieo_ops.ttl --input ${tmpdir}/ieo_entities.ttl annotate --annotation rdfs:comment "This is an extract of the Common Core Ontologies: https://github.com/CommonCoreOntology/CommonCoreOntologies " --output src/imports/cco-extracted.ttl
 
 java -jar robot.jar annotate --input ${imports}/cco-extracted.ttl --ontology-iri "${iri_base}${cco_new_iri}/cco-extracted.ttl" --version-iri "${iri_base}${cco_new_version_iri}/cco-extracted.ttl" --annotation http://purl.org/dc/terms/license https://opensource.org/licenses/BSD-3-Clause --output ${imports}/cco-extracted.ttl
